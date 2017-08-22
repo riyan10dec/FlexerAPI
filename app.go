e789389864fb166f0875a4c072129c7c010bcd33
Riyan Hutomo <riyan10dec2@gmail.com>
2017-08-22 22:58:27 +0700

    add cors

--- app.go
+++ app.go
@@ -655,6 +655,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(code)
 	w.Write(response)
 }
