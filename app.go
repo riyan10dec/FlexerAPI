2711d327a37b4d333dc2938c7d3f039094cdaf48
Riyan Hutomo <riyan10dec2@gmail.com>
2017-08-26 00:08:59 +0700

    add authorization allowed header

--- app.go
+++ app.go
@@ -64,7 +64,7 @@ func (a *App) Initialize() { //user, password, host, port, dbname, screenshotSto
 //RUN
 func (a *App) Run(addr string) {
 	OriginObj := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
-	HeadersObj := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	HeadersObj := handlers.AllowedHeaders([]string{"X-Requested-With", "Authorization"})
 	MethodsObj := handlers.AllowedMethods([]string{"GET", "HEAD", "PUT", "OPTIONS"})
 	log.Fatal(http.ListenAndServe(addr, handlers.CORS(OriginObj, HeadersObj, MethodsObj)(a.Router)))
 }
